51033794cf3b02828c401722780b94dc1a8a7af0
UniQuizzCloud <134231685+UniQuizzCloud@users.noreply.github.com>
2023-12-14 19:02:28 +0000

    Update

--- server.js
+++ server.js
@@ -6,17 +6,57 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the "public" directory
 app.use(express.static('public'));
 
-// Define a route handler for the root route
-app.get('/', (req, res) => {
-  res.send('Hello, this is your server!');
+const rooms = {};
+
+io.on('connection', (socket) => {
+    socket.on('joinRoom', (roomCode) => {
+        socket.join(roomCode);
+
+        // Check if the room exists, if not, create random letters for it
+        if (!rooms[roomCode]) {
+            rooms[roomCode] = generateRandomLetters();
+        }
+
+        // Emit the random letters to everyone in the room
+        io.to(roomCode).emit('updateLetters', rooms[roomCode]);
+    });
+
+    socket.on('createRoom', () => {
+        const roomCode = generateRoomCode();
+        socket.join(roomCode);
+        rooms[roomCode] = generateRandomLetters();
+
+        // Emit the random letters to the creator
+        socket.emit('updateLetters', rooms[roomCode]);
+
+        // Broadcast the new room code to everyone in the room
+        io.to(roomCode).emit('updateRoomCode', roomCode);
+    });
+
+    socket.on('disconnect', () => {
+        // Handle disconnection if needed
+    });
 });
 
-// ... other route handlers or socket.io logic
+function generateRoomCode() {
+    const length = 4;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+function generateRandomLetters() {
+    const length = 6;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]);
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
